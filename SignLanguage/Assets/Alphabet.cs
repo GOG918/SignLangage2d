341ae6cc3e2a7953e452a85f15b45c58302ac29d
GOG918 <83781809+GOG918@users.noreply.github.com>
2023-02-22 21:21:33 +0100

    alphabet ready

--- SignLanguage/Assets/Alphabet.cs
+++ SignLanguage/Assets/Alphabet.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 
 public class Alphabet : MonoBehaviour
@@ -15,6 +16,7 @@ public class Alphabet : MonoBehaviour
     [SerializeField] private Button button3;
     [SerializeField] private Button suivant;
     [SerializeField] private Button Play;
+    [SerializeField] private Button quit;
     [SerializeField] private Text suivantText;
     [SerializeField]private Text AlphabetText;
     [SerializeField] private Text Score;
@@ -22,30 +24,20 @@ public class Alphabet : MonoBehaviour
     [SerializeField] private Text FinishScore;
     [SerializeField] private Text NonFinishScore;
     [SerializeField] private Sprite[] Images;
-    System.Random rng = new System.Random();
-    string[] Alphabets = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W","X", "Y", "Z" };
+    
+    string[] Alphabets = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W","X", "Y", "Z"};
     List<String> AlphabetUse = new List<string>();
     List<string> AlphabetMelange = new List<string>();
     private int score;
-    // Start is called before the first frame update
-    void Start()
-    { 
-       if (suivantText.text == "suivant")
-        {
-            Play.onClick.AddListener(() => Mystart(0, 0, 0, 0, 0, 0));
-            suivant.onClick.AddListener(() => Mystart(0, 0, 0, 0, 0, 0));
-            button1.onClick.AddListener(() => ButtonClick(button1));
-            button2.onClick.AddListener(() => ButtonClick(button2));
-            button3.onClick.AddListener(() => ButtonClick(button3));
-            suivant.interactable = false;
-        }
-       
+    private IEnumerator coroutine;
+    private bool Gamefinish;
 
-    }
-    
-    // Update is called once per frame
+    // Start is called before the first frame update
+
+     
     void Update()
     {
+        
         if (Etat.text == "Vrai")
         {
             button1.interactable = false;
@@ -65,55 +57,83 @@ public class Alphabet : MonoBehaviour
         {
             suivantText.text = "Terminer";
         }
-        if (suivantText.text == "Terminer")
+        
+    }
+
+    public void MystartOnclick()
+    {
+        if (suivantText.text == "suivant") {
+            Mystart(0, 0, 0, 0, 0, 0);
+            Debug.Log(suivantText.text);
+        }else if(suivantText.text == "Terminer")
         {
-            suivantText.text = "suivant";
             Finish.SetActive(true);
             FinishScore.text = Score.text;
+            suivantText.text = "suivant";
+            Debug.Log(suivantText.text);
+        }
+    }
+
+    public void ChoiceButton(Button choiceButton)
+    {
+        if (suivantText.text == "suivant")
+        {
+            ButtonClick(choiceButton);
+            suivant.interactable = false;
         }
         
     }
-    
+    public void home()
+    {
+        Mystart(0, 0, 0, 0, 0, 0);
+        score = 0;
+        Score.text = score.ToString();
+        suivantText.text = "suivant";
+    }
+
     public void Mystart(int j,int  k, int l,int randomIndexAlphabet,int randomValue1, int randomValue2)
     {
         Etat.text = "";
+        System.Random rng = new System.Random();
         button1.interactable = true;
         button2.interactable = true;
         button3.interactable = true;
         suivant.interactable = false;
         bool alphabetBool = false;
-        
-            while (randomIndexAlphabet == randomValue1 || randomIndexAlphabet == randomValue2 || randomValue1 == randomValue2 || alphabetBool == false)
+
+        //while (randomIndexAlphabet == randomValue1 || randomIndexAlphabet == randomValue2 || randomValue1 == randomValue2 || alphabetBool == false)
+        while (randomValue1 == randomValue2 || randomValue1== randomIndexAlphabet || randomValue2 == randomIndexAlphabet || alphabetBool == false)
         {
                 randomIndexAlphabet = rng.Next(0, Alphabets.Length);
                 randomValue1 = rng.Next(0, Alphabets.Length);
                 randomValue2 = rng.Next(0, Alphabets.Length);
-                if (!AlphabetUse.Contains(Alphabets[randomIndexAlphabet]))
-                {
-                    AlphabetText.text = Alphabets[randomIndexAlphabet];
-                    AlphabetUse.Add(Alphabets[randomIndexAlphabet]);
-                    alphabetBool = true;
-                break;
+
+            if (!AlphabetUse.Contains(Alphabets[randomIndexAlphabet]))
+            {
+                AlphabetText.text = Alphabets[randomIndexAlphabet];
+                AlphabetUse.Add(Alphabets[randomIndexAlphabet]);
+                alphabetBool = true;
             }
             else
             {
                 alphabetBool = false;
-                
             }
-            }
+        }
         
+
         int[] tab = { randomIndexAlphabet, randomValue1, randomValue2 };
-        while (j == k || k == l || l == j)
+        while (j == k || k == l || l == j )
         {
             l = rng.Next(0, tab.Length);
             j = rng.Next(0, tab.Length);
             k = rng.Next(0, tab.Length);
-
         }
+
         button1.image.sprite = Images[tab[l]];
         button2.image.sprite = Images[tab[j]];
         button3.image.sprite = Images[tab[k]];
     }
+
     public void ButtonClick(Button buttonclicked)
     {
         if (buttonclicked.image.sprite.name == AlphabetText.text)
